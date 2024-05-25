4f548b586ba8036d8591d776ccfa54b9123afc53
Rajgupta36 <raj.gupta369@outlook.com>
2024-05-25 13:01:21 +0530

    dockerized the application

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,15 +4,15 @@ import { app } from "./app.js"
 
 //This will make sure that all the env variable are loaded once the app is started
 dotenv.config({
-    path:"./.env"
+    path: "./.env"
 })
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log("Server is running at",process.env.PORT)
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("Server is running at", process.env.PORT)
+        })
     })
-})
-.catch((e)=>{
-    console.log("MONGODB connection error",e);
-})
+    .catch((e) => {
+        console.log("MONGODB connection error", e);
+    })
